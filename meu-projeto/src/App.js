1bdeb44a162d329b7910dc67dd0ba9d19d4eda23
EmanuelPS8 <97064304+EmanuelPS8@users.noreply.github.com>
2025-03-21 16:33:34 -0300

    estudar

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 function App() {
+
+  // pode utilizar esse área para colocar variaveis
+  const nome = "Emanuel";
+
+
+
+  const url = "https://i.pinimg.com/736x/c8/49/6f/c8496f276336e9e3ce63462ea791f9c2.jpg";
+  const url2 = "https://i.pinimg.com/736x/f6/7f/e9/f67fe9486563e6077072a61d45c4140c.jpg"
+
   return (
     <div className="App">
       <h1>Olá, mundo!</h1>
-      <p>Este é o meu primeiro aplicativo React.</p>
-      <img src={logo} className="App-logo" alt="logo" />
+      <Frase />
+      <Frase />
+      <p>Olá, {nome}.</p>
+      <img src={url} alt="Imagem de exemplo"/>
+      <SayMyName nome="Emanuel" />
+      <SayMyName nome="João" />
+      <SayMyName nome={nome} />
+      <Pessoa foto={url2} nome="JungKook" idade="28" profissao="Desenvolvedor" />
     </div>
   );
 }
